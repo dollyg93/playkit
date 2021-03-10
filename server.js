39e76d52f2b23e6eec9278568d72c68b5aa7335c
Dolly Garg <dollygarg@Dollys-MacBook-Air.local>
2021-03-11 01:38:02 +0530

    updates to deploy app to heroku

---
+++ server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+
+var distDir = './dist/';
+
+app.use(express.static(path.join(__dirname, distDir)))
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, distDir + '/index.html'));
+});
+
+console.log(distDir);
+
+app.use((req, res, next) => {
+  const err = new httpError(404)
+  return next(err);
+});
+
+app.use((err, req, res, next) => {
+
+  res.status(err.status || 500).json({
+    message: err.message
+  });
+  next(err);
+});
+
+app.listen(3000, () => {
+    console.info(`server started on port 3000)`);
+});
+
+module.exports = app;
